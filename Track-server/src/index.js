62dca6cdc738b9ae05fb5189750ad926d02e6daa
Tushar Jain <tusharlock10@gmail.com>
2023-05-10 01:06:25 +0530

    added route

--- Track-server/src/index.js
+++ Track-server/src/index.js
@@ -1,6 +1,22 @@
+const mongoose = require('mongoose')
 const express = require('express')
+const authRoutes = require('./routes/authRoutes')
+
+const mongoUri = 'mongodb+srv://ishaanjainlock10:0810200A@cluster0.7gmouvx.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.connect(mongoUri)
+mongoose.connection.on('connected',() => {
+  console.log('Connected to mongoose instance')
+})
+mongoose.connection.on('error', (err) => {
+  console.log('Error connecting to mongo', err)
+})
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(authRoutes)
 
 app.get('/', (req,res) => {
   res.send('Hello! My first message from express API!!!')
