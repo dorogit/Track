7d15933f713aeddb60acfa0d76047f460df2ccac
Ishaan <ishaanjainlock10@gmail.com>
2023-05-20 03:49:51 +0530

    navigator wip

--- Track-app/App.js
+++ Track-app/App.js
@@ -1,16 +1,39 @@
 import React from "react";
 import SignUp from "./src/screens/SignUp";
+import SignIn from "./src/screens/SignIn";
+import CreateScreen from "./src/screens/CreateScreen";
+import DetailScreen from "./src/screens/DetailScreen";
+import ListScreen from "./src/screens/ListScreen";
+import AccountScreen from "./src/screens/AccountScreen";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
 const Stack = createNativeStackNavigator()
+const Tab = createMaterialBottomTabNavigator()
+
+const Authentication = () => (
+  <Stack.Navigator>
+    <Stack.Screen name = "SignUp" component={SignUp} />
+    <Stack.Screen name = "SignIn" component={SignIn} />
+  </Stack.Navigator>
+)
+
+const HomeStack = () => (
+  <Tab.Navigator>
+    <Stack.Navigator>
+      <Stack.Screen name = "List" component={ListScreen} />
+      <Stack.Screen name = "Detail" component={DetailScreen} />
+    </Stack.Navigator>
+    <Stack.Screen name = "Create" component={CreateScreen} />
+    <Stack.Screen name = "Account" component={AccountScreen} />
+  </Tab.Navigator>
+)
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="SignUp" component={SignUp} />
-      </Stack.Navigator>
+      <Authentication />
     </NavigationContainer>
   )
 }
---
+++ Track-app/src/screens/AccountScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text,View } from "react-native";
+
+const AccountScreen = () => {
+    return (
+        <View>
+            <Text>hiii111</Text>
+        </View>
+    )
+}
+
+export default AccountScreen
