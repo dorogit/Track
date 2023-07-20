042cc98f3ec91a51a3723590cd41e6e5242e7a0d
Ishaan <ishaanjainlock10@gmail.com>
2023-07-20 17:32:46 +0530

    Update CreateScreen.js

--- Track-app/src/screens/CreateScreen.js
+++ Track-app/src/screens/CreateScreen.js
@@ -1,12 +1,30 @@
-import React from "react";
-import { Text,View } from "react-native";
+import React from 'react';
+import { StyleSheet,SafeAreaView } from 'react-native';
+import { Text } from 'react-native-elements';
+import MapView, {Polyline} from 'react-native-maps';
 
 const CreateScreen = () => {
-    return (
-        <View>
-            <Text>This is the Create Screen !!</Text>
-        </View>
-    )
-}
+  return (
+    <SafeAreaView forceInset={{ top: 'always' }}>
+      <Text h2>Create a Track</Text>
+      <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: 28.4598397,
+        longitude: 77.0308019,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+      }}
+    />
+    </SafeAreaView>
+  );
+};
 
-export default CreateScreen
+const styles = StyleSheet.create({
+    map:{
+        width:"100%",
+        height:"100%"
+    }
+});
+
+export default CreateScreen;
